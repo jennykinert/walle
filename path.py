89c17af22b840f101d9dcb93549341894fe22c6e
Jenny Kinert <id14jkt@cs.umu.se>
2016-09-01 17:18:46 +0200

    Edited Path class. Added functionality lika get prosition and read file

--- path.py
+++ path.py
@@ -1,4 +1,23 @@
-"""
-Class that reads position data
-Functionality: Gets the requested position
-"""
+class Path:
+    """
+    Class that reads position data
+    Functionality: Gets the requested position
+    """
+    def __init__(self, path):
+        """Construct the path class. Reads the path file and extract the information
+        """
+        self.path = path
+        if self.path == None:
+            raise ValueError
+        else:
+            with open(self.path, "r") as file:
+                self.information = json.load(file)
+
+    def get_position(self,n):
+        """
+        Returns a specific position on the path
+        """
+        position = self.information[n]
+        x = position["Pose"]["Position"]["X"]
+        y = position["Pose"]["Position"]["Y"]
+        return x,y
